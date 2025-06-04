4f1b6f4f6ecd47ff4279b88bcbc1dc9fb1814463
sijun <sionwer5@gmail.com>
2025-06-04 22:42:52 +0900

    feat: Create project

---
+++ src/test/kotlin/com/parsimony/gamewiki/GamewikiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parsimony.gamewiki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamewikiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
